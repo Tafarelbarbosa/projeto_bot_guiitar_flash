6d3de8c06030fbb738456ae250f4c83e033b1f35
Tafarel Barbosa <tafarelhenrique@yahoo.com>
2024-12-02 20:54:47 -0300

    Criado a biblioteca webbrowser e o comando para abrir o site guitarflash

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import pyautogui
 from time import sleep
 import pyperclip
+import webbrowser
 
 # 1 Abrir a página https://guitarflash.com
-# 2 Escolher a música 
-# 3 Clicar na música
-# 4 Apertar uma tecla para começar
-# 5 Se verificar quando será necessário apertar os botões a,s,j,k,l
+webbrowser.open_new_tab('https://guitarflash.com')
+# 2  Clicar em jogar
+# 3 Escolher a música 
+# 4 Clicar na música
+# 5 Apertar uma tecla para começar
+# 6 Se verificar quando será necessário apertar os botões a,s,j,k,l
