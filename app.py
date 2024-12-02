3c39947b3601ea38dc8b42ebe0b4b613fb337388
Tafarel Barbosa <tafarelhenrique@yahoo.com>
2024-12-02 20:51:15 -0300

    Importado as bibliotecas que serão utilizadas no código

---
+++ app.py
@@ -0,0 +1,9 @@
+import pyautogui
+from time import sleep
+import pyperclip
+
+# 1 Abrir a página https://guitarflash.com
+# 2 Escolher a música 
+# 3 Clicar na música
+# 4 Apertar uma tecla para começar
+# 5 Se verificar quando será necessário apertar os botões a,s,j,k,l
