0d2e26f42b51cb10663d20ed916e96c4a5edb078
Tafarel Barbosa <tafarelhenrique@yahoo.com>
2024-12-02 22:41:06 -0300

    Criado a função mover_janela_cima, e criado os comandos necessários para achar a música e clicar para selecionar

--- app.py
+++ app.py
@@ -12,11 +12,24 @@ def mover_janela_direita():
     sleep(1)
     pyautogui.keyUp('win')   
     
+'''def mover_janela_cima():
+    pyautogui.keyDown('win')
+    sleep(1)
+    pyautogui.keyDown('up')
+    sleep(1)
+    pyautogui.keyUp('up')
+    sleep(1)
+    pyautogui.keyDown('up')
+    sleep(1)
+    pyautogui.keyUp('up')
+    sleep(1)
+    pyautogui.keyUp('win')'''
 
 # # 1 Abrir a página https://guitarflash.com
 webbrowser.open_new_tab('https://guitarflash.com')
 sleep(2)
 mover_janela_direita()
+
 # 2  Clicar em jogar
 sleep(2)
 botao_curtir = (pyautogui.locateCenterOnScreen('proj2aula17/botao_curtir.png'))
@@ -24,7 +37,16 @@ sleep(2)
 pyautogui.moveTo(botao_curtir[0],botao_curtir[1],duration=2)
 pyautogui.move(0,62,duration=1)
 pyautogui.click()
-# 3 Escolher a música 
-# 4 Clicar na música
-# 5 Apertar uma tecla para começar
-# 6 Se verificar quando será necessário apertar os botões a,s,j,k,l
+
+# 3 Escolher a música e clicar na música
+sleep(2)
+pyautogui.hotkey('ctrl','f')
+sleep(1)
+pyautogui.typewrite('psycho killer')
+musica = (pyautogui.locateCenterOnScreen('proj2aula17/musica.png'))
+sleep(2)
+pyautogui.click(musica[0],musica[1],duration=2)
+sleep(1)
+
+# 6 Rolar 400 pixels para baixo e apertar uma tecla para começar
+# 7 Se verificar quando será necessário apertar os botões a,s,j,k,l
